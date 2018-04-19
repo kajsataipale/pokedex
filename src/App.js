bbe20a81f516e976653ac8c5116f9e4574ea8a24
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2018-04-19 13:18:50 +0200

    added sections

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Pokedex</h1>
         </header>
-        <PokeDex />
+        <div class="section-one">
+        </div>
+        <div class="section-two">
+          <PokeDex />
+        </div>
       </div>
     );
   }
